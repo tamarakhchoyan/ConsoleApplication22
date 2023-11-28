7b337c3d0326ae2047b28a07722b20eee3c50de9
User <User@DESKTOP-9TGTEUB>
2023-11-28 11:21:01 +0400

    Add project files.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> d;
+	std::cout << a << "-" << b << "-" << c << "-" << d << std::endl;
+	return 0;
+
+}
